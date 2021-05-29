b1d0b5fd411b3e9561d31c1cf19aeaa88c1905b1
Victors1967 <victorsmirnov@me.com>
2021-05-29 17:07:06 +0300

    add component and services

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_URL: 'https://api.rawg.io/api',
+  API_KEY: 'f959a9792af34a0c9ba6cea238ac34b1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  api_key: 'f959a9792af34a0c9ba6cea238ac34b1'
+  BASE_URL: 'https://api.rawg.io/api',
+  API_KEY: 'f959a9792af34a0c9ba6cea238ac34b1'
 };
 
 /*
